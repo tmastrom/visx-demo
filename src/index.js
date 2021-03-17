d8f487aa9ce21307280f4d7d8ade6d2793551708
tmastrom <thomas.mastromonaco@gmail.com>
2021-03-17 10:25:45 -0700

    first example

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import ParentSize from "@visx/responsive/lib/components/ParentSize";
+
+import TemperatureBarStack from "./TemperatureBarStack";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  
+    <ParentSize>
+      {parent => (
+        <TemperatureBarStack
+          width={ parent.width }
+          height={ parent.height }
+        />
+      )}
+    </ParentSize>
+  ,
+  document.getElementById("root")
+);
